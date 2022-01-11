4a26fc967231bc28c6a49de3e7b8a69e57b699c0
zhengxingXue <zx252cornell@gmail.com>
2022-01-11 14:30:15 -0500

    add css files and logos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import './App.css';
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+const dockerLogoURL =
+  'https://www.docker.com/sites/default/files/d8/2019-07/vertical-logo-monochromatic.png';
+const k8sLogoURL =
+  'https://logos-download.com/wp-content/uploads/2018/09/Kubernetes_Logo.png';
+
 const App = () => {
   return (
-    <div>
-      <h1>hello World</h1>
+    <div className='App'>
+      <h1>Hello World</h1>
+      <img className='App-tech-img' src={dockerLogoURL} alt='docker logo' />
+      <img className='App-tech-img' src={k8sLogoURL} alt='k8s logo' />
+      <p>&copy; {year}, Zhengxing Xue</p>
     </div>
   );
 };
