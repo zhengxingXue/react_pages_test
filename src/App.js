b7d9da5744a4a5665dd2a2fba1b34527a63ec668
zhengxingXue <zx252cornell@gmail.com>
2022-01-13 01:03:17 -0500

    use material ui setup template

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import './App.css';
-import Heading from './components/Heading';
-import DockerLogo from './components/TechLogos/DockerLogo';
-import K8sLogo from './components/TechLogos/K8sLogo';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
 
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-
-const App = () => {
+function Copyright() {
   return (
-    <div className='App'>
-      <Heading />
-      <DockerLogo />
-      <K8sLogo />
-      <p>&copy; {year}, Zhengxing Xue</p>
-    </div>
+    <Typography variant='body2' color='text.secondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='https://mui.com/'>
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
-};
+}
 
-export default App;
+export default function App() {
+  return (
+    <Container maxWidth='sm'>
+      <Box sx={{ my: 4 }}>
+        <Typography variant='h4' component='h1' gutterBottom>
+          Create React App example
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
